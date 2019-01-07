95c92c28e1ff70279978c22603266fa0b9f3851b
5ia17jowaeger <5ia17jowaeger@gibwintra.ch>
2019-01-07 08:17:19 +0100

    correct some errors and add the summe

--- src/main/java/ch/bbw/Console_Calculator/App.java
+++ src/main/java/ch/bbw/Console_Calculator/App.java
@@ -7,21 +7,9 @@ import java.util.Scanner;
  *
  */
 public class App {
-	double result = 0; 
 	public static void main(String[] args) {
 
-		Calculator calculator = new Calculator();
-		int valueA = 0;
-		int valueB = 0;
 		
-		System.out.println("Console Calculator");
-		System.out.println("==================");
-		System.out.println();
-		
-		
-		valueA = 10;
-		valueB = 20;
-		System.out.println("Summe" + valueA + " + " + valueB + " = " + calculator.summe(10, 20));
 			
 	}
 
--- src/main/java/ch/bbw/Console_Calculator/Calculator.java
+++ src/main/java/ch/bbw/Console_Calculator/Calculator.java
@@ -2,6 +2,7 @@ package ch.bbw.Console_Calculator;
 
 public class Calculator {
 
+	
 	public double summe( double summand1, double summand2) {
 		
 		return summand1 + summand2;
